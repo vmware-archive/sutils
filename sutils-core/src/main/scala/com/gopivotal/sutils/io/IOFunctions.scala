b213395921cedc55e69930fb1fe86f32f0ffbef4
David Capwell <dcapwell@davidcapwellmbp.attlocal.net>
2014-10-14 21:53:38 -0700

    fixed close bug

--- sutils-core/src/main/scala/com/gopivotal/sutils/io/IOFunctions.scala
+++ sutils-core/src/main/scala/com/gopivotal/sutils/io/IOFunctions.scala
@@ -3,10 +3,13 @@ package com.gopivotal.sutils.io
 import java.io.Closeable
 
 trait IOFunctions {
-  def close[A <: Closeable, B](a: =>A)(fn: A => B): B = try {
-    fn(a)
-  } finally {
-    a.close()
+  def close[A <: Closeable, B](a: =>A)(fn: A => B): B = {
+    lazy val alocal = a
+    try {
+      fn(alocal)
+    } finally {
+      alocal.close()
+    }
   }
 
   def close[A <: Closeable, B <: Closeable, C](a: => A, b: => B)(fn: (A, B) => C): C = {
